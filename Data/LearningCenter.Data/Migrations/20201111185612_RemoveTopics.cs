17fa3b78efaaab22db374d9ee05ce9f0bbd2e3fc
Nikolay <sennawalker777@gmail.com>
2020-11-12 00:32:50 +0200

    CSS changes

---
+++ Data/LearningCenter.Data/Migrations/20201111185612_RemoveTopics.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LearningCenter.Data.Migrations
+{
+    public partial class RemoveTopics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Topics_TopicId",
+                table: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Topics");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_TopicId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "TopicId",
+                table: "Courses");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TopicId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Topics_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Topics_SubCategories_SubCategoryId",
+                        column: x => x.SubCategoryId,
+                        principalTable: "SubCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_TopicId",
+                table: "Courses",
+                column: "TopicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Topics_CategoryId",
+                table: "Topics",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Topics_IsDeleted",
+                table: "Topics",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Topics_SubCategoryId",
+                table: "Topics",
+                column: "SubCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Topics_TopicId",
+                table: "Courses",
+                column: "TopicId",
+                principalTable: "Topics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
