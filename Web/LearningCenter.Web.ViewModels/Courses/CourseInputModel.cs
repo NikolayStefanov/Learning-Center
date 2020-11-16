48abca0d180043918072f7ac830d5aade1611442
Nikolay <sennawalker777@gmail.com>
2020-11-16 23:18:20 +0200

    Courses Controller added

---
+++ Web/LearningCenter.Web.ViewModels/Courses/CourseInputModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningCenter.Web.ViewModels.Courses
+{
+    public class CourseInputModel
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(300)]
+        public string Description { get; set; }
+
+        [Required]
+        public int LanguageId { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int SubcategoryId { get; set; }
+    }
+}
--- Web/LearningCenter.Web/Controllers/CoursesController.cs
+++ Web/LearningCenter.Web/Controllers/CoursesController.cs
@@ -1,8 +1,24 @@
 ﻿namespace LearningCenter.Web.Controllers
 {
+    using LearningCenter.Web.ViewModels.Courses;
     using Microsoft.AspNetCore.Mvc;
 
     public class CoursesController : Controller
     {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CourseInputModel inputModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(inputModel);
+            }
+
+            return this.RedirectToAction(nameof(this.Create));
+        }
     }
 }
